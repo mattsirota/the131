4464bc7d76ddf1698dde0c6f12e7cfbdeda82690
Gabi Tantillo <gtantill@stevens.edu>
2020-04-15 15:28:29 -0400

    spring web service

--- ezforms/src/main/java/main.java
+++ ezforms/src/main/java/main.java
@@ -1,7 +1,8 @@
-//import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication;
 
+@SpringBootApplication
 public class main {
     public static void main(String [] args){
-        //SpringApplication.run(main.class, args);
+        SpringApplication.run(main.class, args);
     }
 }
